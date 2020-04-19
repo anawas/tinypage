64b8a036a13a0fb78c14e104270e6d4233c2daf9
anawas <aw@andreas-wassmer.ch>
2020-04-19 11:04:47 +0200

    Bug fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $public_name = @file_get_contents("http://instance-data/latest/meta-data/public-
 $instance_type = @file_get_contents("http://instance-data/latest/meta-data/instance-type");
 $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
 
-echo "<h2 style='colr:blue;'>Welcome</h2><br />";
+echo "<h2 style='color:blue;'>Welcome</h2><br />";
 echo "Running on host:  " . $hostname . "<br />";
 echo "Public dns name:  " . $public_name . "<br />";
 echo "Instance of type: " . $instance_type . "<br />";
