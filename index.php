1706cd16ff048ea793f0a1f790ead11ac6cf657e
anawas <aw@andreas-wassmer.ch>
2020-04-19 10:47:48 +0200

    Added more meta-data

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $hostname = @file_get_contents("http://instance-data/latest/meta-data/local-hostname");
+$public_name = @file_get_contents("http://instance-data/latest/meta-data/public-hostname");
 $instance_type = @file_get_contents("http://instance-data/latest/meta-data/instance-type");
-$metrics = @file_get_contents("http://instance-data/latest/meta-data/metrics");
+$instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
 
-echo "Running on host: " . $hostname . "<br>";
-echo "It is an instance of type " . $instance_type . "<br>";
-echo "Important metrics: " . $metrics . "<br>"; 
+echo "Running on host:  " . $hostname . "<br />";
+echo "Public dns name:  " . $public_name . "<br />"
+echo "Instance of type: " . $instance_type . "<br />";
+echo "Instance id:      " . $instance_id . "<br />";
 ?>
