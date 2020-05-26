60fd32c5329a8c9c673b4f588d4a8a8ddd72be1b
anawas <aw@andreas-wassmer.ch>
2020-05-26 13:52:52 +0200

    Bigger text

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $public_name = @file_get_contents("http://instance-data/latest/meta-data/public-
 $instance_type = @file_get_contents("http://instance-data/latest/meta-data/instance-type");
 $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
 
-echo "<h2 style='color:blue;'>Welcome</h2><br />";
-echo "Running on host:  " . $hostname . "<br />";
-echo "Public dns name:  " . $public_name . "<br />";
+echo "<h1 style='color:green;'>Welcome</h1><br />";
+echo "<h3>Running on host:  " . $hostname . "</h3><br />";
+echo "<h3>Public dns name:  " . $public_name . "</h3><br />";
 echo "Instance of type: " . $instance_type . "<br />";
 echo "Instance id:      " . $instance_id . "<br />";
 ?>
