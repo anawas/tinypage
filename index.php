96e44e7a3976743e422d1f09e7737abed28a9b81
anawas <aw@andreas-wassmer.ch>
2020-05-26 14:09:49 +0200

    div instead of <hx> gives better control

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $instance_type = @file_get_contents("http://instance-data/latest/meta-data/insta
 $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
 
 echo "<h1 style='color:green;'>Welcome</h1><br />";
-echo '<div style="font-size: 130%;">Running on host:  ' . $hostname . '</div><br />';
-echo '<div style="font-size: 130%;">Public dns name:  ' . $public_name . '</div><br />';
+echo "<div style='font-size: 130%;'>Running on host: " . $hostname . "</div><br />";
+echo "<div style='font-size: 130%;'>Public dns name: " . $public_name . "</div><br />";
 echo "Instance of type: " . $instance_type . "<br />";
 echo "Instance id:      " . $instance_id . "<br />";
 ?>
