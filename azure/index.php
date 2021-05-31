6df35286e5e8153a64b3737fb50980dff7b864ab
Andreas Wassmer <andreas.wassmer@me.com>
2021-05-31 10:18:17 +0200

    Finished version of Azure tinypage

--- azure/index.php
+++ azure/index.php
@@ -1,15 +1,32 @@
+
 <?php
-$hostname = @file_get_contents("http://instance-data/latest/meta-data/local-hostname");
-$public_name = @file_get_contents("http://instance-data/latest/meta-data/public-hostname");
-$instance_type = @file_get_contents("http://instance-data/latest/meta-data/instance-type");
-$instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
+$opts = array(
+  'http'=>array(
+    'header'=>"Metadata: true\r\n"
+  )
+);
+
+$context = stream_context_create($opts);
+$metadata = file_get_contents("http://169.254.169.254/metadata/instance?api-version=2021-02-01", false, $context);
+$m = json_decode($metadata);
+
+$compute = $m->compute;
+$region = $compute->location;
+$hostname = $compute->name;
+$os_type = $compute->osType;
+$vm_size = $compute->vmSize;
+$vm_id = $compute->vmId;
+$netw = $m->network->interface[0]->ipv4->ipAddress[0];
+$public_ip = $netw->publicIpAddress;
 
 echo "<h2 style='color:blue;'>Welcome</h2><br />";
+echo "<h2>";
 echo "Running on host:  " . $hostname . "<br />";
-echo "Public dns name:  " . $public_name . "<br />";
-echo "Instance of type: " . $instance_type . "<br />";
-echo "Instance id:      " . $instance_id . "<br />";
+echo "      in region:  " . $region . "<br />";
+echo "OS type        :  " . $os_type . "<br />";
+echo "VM size        :  " . $vm_size . "<br />";
+echo "VM id          :  " . $vm_id . "<br />";
+echo "Public IP      :  " . $public_ip . "<br />";
+echo "</h2>";
+
 ?>
-
-
-
--- terraform_azure.tf
+++ terraform_azure.tf
@@ -71,11 +71,22 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1010
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "Terraform Demo"
     }
 }
-
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
     name                      = "myNIC"
