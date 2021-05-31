1659da845cb9f472ad8ef41454b14a2b142df0e5
Andreas Wassmer <andreas.wassmer@me.com>
2021-05-31 08:07:54 +0200

    New structure to support deployment on azure

---
+++ azure/index.php
@@ -0,0 +1,15 @@
+<?php
+$hostname = @file_get_contents("http://instance-data/latest/meta-data/local-hostname");
+$public_name = @file_get_contents("http://instance-data/latest/meta-data/public-hostname");
+$instance_type = @file_get_contents("http://instance-data/latest/meta-data/instance-type");
+$instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
+
+echo "<h2 style='color:blue;'>Welcome</h2><br />";
+echo "Running on host:  " . $hostname . "<br />";
+echo "Public dns name:  " . $public_name . "<br />";
+echo "Instance of type: " . $instance_type . "<br />";
+echo "Instance id:      " . $instance_id . "<br />";
+?>
+
+
+
